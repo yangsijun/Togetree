39e7f21162ab2523666074d4ccc54c326fc85e14
Yang Sijun <yang@sijun.dev>
2025-04-14 17:09:38 +0900

    Add MainView

---
+++ Projects/Togetree/Sources/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  Togetree
+//
+//  Created by 양시준 on 4/14/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
--- Projects/Togetree/Sources/TogetreeApp.swift
+++ Projects/Togetree/Sources/TogetreeApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct TogetreeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
