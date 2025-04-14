6bb42ff74343bb66891d8565a198e1084b9d941f
Yang Sijun <yang@sijun.dev>
2025-04-14 19:53:39 +0900

    Add User Model

---
+++ Projects/Togetree/Sources/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Togetree
+//
+//  Created by 양시준 on 4/14/25.
+//
+
+import Foundation
+
+public struct User: Codable {
+    public var id: UUID = UUID()
+    public var name: String
+    public var profileImageUrl: String?
+}
