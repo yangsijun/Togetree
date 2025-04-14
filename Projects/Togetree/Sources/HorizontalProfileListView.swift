e063f95f31053590be9b0748119d6e93006bb4d7
Yang Sijun <yang@sijun.dev>
2025-04-14 19:54:21 +0900

    Create HorizontalProfileListView

---
+++ Projects/Togetree/Sources/HorizontalProfileListView.swift
@@ -0,0 +1,49 @@
+//
+//  HorizontalProfileListView.swift
+//  Togetree
+//
+//  Created by 양시준 on 4/14/25.
+//
+
+import SwiftUI
+
+struct HorizontalProfileListView: View {
+    var userList: [User]
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(userList, id: \.id) { user in
+                    UserProfileView(user: user, imageSize: 64)
+                }
+                Button(action: {
+                    // TODO: Navigate to FollowAndFollowerView
+                }) {
+                    Image(systemName: "chevron.forward.circle")
+                }
+                .padding(.leading, 8)
+            }
+            .padding(.vertical, 10)
+            .padding(.horizontal, 20)
+        }
+        .scrollIndicators(.hidden)
+    }
+}
+
+struct HorizontalProfileListView_Previews: PreviewProvider {
+    static var previews: some View {
+        HorizontalProfileListView(
+            userList: [
+                User(name: "Air", profileImageUrl: "https://picsum.photos/200/300"),
+                User(name: "Air", profileImageUrl: "https://picsum.photos/200/300"),
+                User(name: "Air", profileImageUrl: "https://picsum.photos/200/300"),
+                User(name: "Air", profileImageUrl: "https://picsum.photos/200/300"),
+                User(name: "Air", profileImageUrl: "https://picsum.photos/200/300"),
+                User(name: "Air", profileImageUrl: "https://picsum.photos/200/300"),
+                User(name: "Air", profileImageUrl: "https://picsum.photos/200/300"),
+                User(name: "Air", profileImageUrl: "https://picsum.photos/200/300"),
+                User(name: "Air", profileImageUrl: "https://picsum.photos/200/300"),
+            ]
+        )
+    }
+}
