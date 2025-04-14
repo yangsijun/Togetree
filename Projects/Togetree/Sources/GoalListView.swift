7bc04d7016ab587c9ea699793a650895f0a9494f
Yang Sijun <yang@sijun.dev>
2025-04-14 23:20:54 +0900

    Add GoalListView

---
+++ Projects/Togetree/Sources/GoalListView.swift
@@ -0,0 +1,86 @@
+//
+//  GoalListView.swift
+//  Togetree
+//
+//  Created by 양시준 on 4/14/25.
+//
+
+import SwiftUI
+
+struct GoalListView: View {
+    @Binding var goals: [Goal]
+    
+    var body: some View {
+        List {
+            ForEach(goals.indices, id: \.self) { index in
+                switch goals[index].goalType {
+                case .singleGoal:
+                    if let _ = goals[index] as? SingleGoalGoal {
+                        SingleGoalGoalCardView(
+                            goal: Binding<SingleGoalGoal>(
+                                get: { self.goals[index] as! SingleGoalGoal },
+                                set: { self.goals[index] = $0 }
+                            )
+                        )
+                    }
+                case .subGoals:
+                    // TODO: Implement SubGoalsGoalCardView
+                    Text("SubGoals")
+                case .progress:
+                    // TODO: Implement ProgressGoalCardView
+                    Text("Progress")
+                }
+            }
+            .listRowBackground(Color.clear)
+        }
+        .listStyle(.plain)
+        .background(Color("SecondaryBackground"))
+        .scrollContentBackground(.hidden)
+    }
+}
+
+struct GoalListView_Previews: PreviewProvider {
+    @State static var goals: [Goal] = [
+        SingleGoalGoal(
+            title: "앱 배포하기",
+            description: "dfdsdfsdf",
+            createdAt: Date(),
+            updatedAt: Date(),
+            startDate: Calendar.current.date(from: DateComponents(year: 2025, month: 4, day: 1))!,
+            endDate: Calendar.current.date(from: DateComponents(year: 2025, month: 4, day: 30))!,
+            isPublic: false,
+            completed: false
+        ),
+        SubGoalsGoal(
+            title: "챌린지 2",
+            description: "챌린지2를 통해 배우고 성장하기",
+            createdAt: Date(),
+            updatedAt: Date(),
+            startDate: Calendar.current.date(from: DateComponents(year: 2025, month: 4, day: 7))!,
+            endDate: Calendar.current.date(from: DateComponents(year: 2025, month: 4, day: 25))!,
+            isPublic: true,
+            subGoals: [
+                SubGoal(title: "Solution Concept 선택하기", completed: true),
+                SubGoal(title: "App Statement 만들기", completed: true),
+                SubGoal(title: "Feature List 만들기", completed: true),
+                SubGoal(title: "Lofi 그리기", completed: true),
+                SubGoal(title: "Hifi 그리기", completed: false),
+            ]
+        ),
+        ProgressGoal(
+            title: "러닝으로 건강한 몸과 마음 만들기",
+            description: "4월 한 달 동안 3km 러닝 10번 하기",
+            createdAt: Date(),
+            updatedAt: Date(),
+            startDate: Calendar.current.date(from: DateComponents(year: 2025, month: 4, day: 1))!,
+            endDate: Calendar.current.date(from: DateComponents(year: 2025, month: 4, day: 30))!,
+            isPublic: true,
+            currentProgress: 4,
+            endProgress: 10
+        )
+    ]
+    
+    static var previews: some View {
+        GoalListView(goals: $goals)
+    }
+}
--- Projects/Togetree/Sources/MainView.swift
+++ Projects/Togetree/Sources/MainView.swift
@@ -8,19 +8,14 @@
 import SwiftUI
 
 struct MainView: View {
-    var userList: [User] = []
-    var goalList: [Goal] = []
+    @State var userList: [User] = []
+    @State var goalList: [Goal] = []
     
     var body: some View {
         NavigationStack {
             VStack {
                 HorizontalProfileListView(userList: userList)
-                // TODO: Goal List
-                List {
-                    Text("Goal List")
-                    Text("Goal List")
-                    Text("Goal List")
-                }
+                GoalListView(goals: $goalList)
             }
             .navigationTitle(Text("Togetree"))
             .navigationBarTitleDisplayMode(.large)
@@ -52,6 +47,45 @@ struct MainView_Previews: PreviewProvider {
                 User(name: "Air", profileImageUrl: "https://picsum.photos/200/300"),
                 User(name: "Air", profileImageUrl: "https://picsum.photos/200/300"),
                 User(name: "Air", profileImageUrl: "https://picsum.photos/200/300"),
+            ],
+            goalList: [
+                SingleGoalGoal(
+                    title: "앱 배포하기",
+                    description: "dfdsdfsdf",
+                    createdAt: Date(),
+                    updatedAt: Date(),
+                    startDate: Calendar.current.date(from: DateComponents(year: 2025, month: 4, day: 1))!,
+                    endDate: Calendar.current.date(from: DateComponents(year: 2025, month: 4, day: 30))!,
+                    isPublic: false,
+                    completed: false
+                ),
+                SubGoalsGoal(
+                    title: "챌린지 2",
+                    description: "챌린지2를 통해 배우고 성장하기",
+                    createdAt: Date(),
+                    updatedAt: Date(),
+                    startDate: Calendar.current.date(from: DateComponents(year: 2025, month: 4, day: 7))!,
+                    endDate: Calendar.current.date(from: DateComponents(year: 2025, month: 4, day: 25))!,
+                    isPublic: true,
+                    subGoals: [
+                        SubGoal(title: "Solution Concept 선택하기", completed: true),
+                        SubGoal(title: "App Statement 만들기", completed: true),
+                        SubGoal(title: "Feature List 만들기", completed: true),
+                        SubGoal(title: "Lofi 그리기", completed: true),
+                        SubGoal(title: "Hifi 그리기", completed: false),
+                    ]
+                ),
+                ProgressGoal(
+                    title: "러닝으로 건강한 몸과 마음 만들기",
+                    description: "4월 한 달 동안 3km 러닝 10번 하기",
+                    createdAt: Date(),
+                    updatedAt: Date(),
+                    startDate: Calendar.current.date(from: DateComponents(year: 2025, month: 4, day: 1))!,
+                    endDate: Calendar.current.date(from: DateComponents(year: 2025, month: 4, day: 30))!,
+                    isPublic: true,
+                    currentProgress: 4,
+                    endProgress: 10
+                )
             ]
         )
     }
