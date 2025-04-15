a7fd83918977c0cd39f3e224e272419211826952
Yang Sijun <yang@sijun.dev>
2025-04-15 15:19:59 +0900

    Create and Implement ProgressGoalCardView Create ProgressGoalDetailView

---
+++ Projects/Togetree/Sources/Views/ProgressGoalCardView.swift
@@ -0,0 +1,53 @@
+//
+//  ProgressGoalCardView.swift
+//  Togetree
+//
+//  Created by 양시준 on 4/15/25.
+//
+
+import SwiftUI
+
+public struct ProgressGoalCardView: View {
+    @Binding var goal: ProgressGoal
+    
+    public var body: some View {
+        NavigationLink {
+            ProgressGoalDetailView(goal: $goal)
+        } label: {
+            VStack(spacing: 8) {
+                GoalCardTextView(title: goal.title, description: goal.description, isPublic: goal.isPublic)
+                GoalProgressBarWithStepperView(
+                    currentProgress: $goal.currentProgress,
+                    endProgress: goal.endProgress,
+                    goalLabel: goal.goalLabel
+                )
+                    .padding(.top, 4)
+            }
+            .padding(20)
+            .background(
+                GoalCardBackgroundView()
+            )
+        }
+        .buttonStyle(.plain)
+    }
+}
+
+struct ProgressGoalCardView_Previews: PreviewProvider {
+    @State static var goal = ProgressGoal(
+        title: "러닝으로 건강한 몸과 마음 만들기",
+        description: "4월 한 달 동안 3km 러닝 10번 하기",
+        createdAt: Date(),
+        updatedAt: Date(),
+        startDate: Calendar.current.date(from: DateComponents(year: 2025, month: 4, day: 1))!,
+        endDate: Calendar.current.date(from: DateComponents(year: 2025, month: 4, day: 30))!,
+        isPublic: true,
+        currentProgress: 4,
+        endProgress: 10
+    )
+    
+    static var previews: some View {
+        NavigationStack {
+            ProgressGoalCardView(goal: $goal)
+        }
+    }
+}
---
+++ Projects/Togetree/Sources/Views/ProgressGoalDetailView.swift
@@ -0,0 +1,34 @@
+//
+//  ProgressGoalDetailView.swift
+//  Togetree
+//
+//  Created by 양시준 on 4/15/25.
+//
+
+import SwiftUI
+
+struct ProgressGoalDetailView: View {
+    @Binding var goal: ProgressGoal
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ProgressGoalDetailView_Previews: PreviewProvider {
+    @State static var goal = ProgressGoal(
+        title: "러닝으로 건강한 몸과 마음 만들기",
+        description: "4월 한 달 동안 3km 러닝 10번 하기",
+        createdAt: Date(),
+        updatedAt: Date(),
+        startDate: Calendar.current.date(from: DateComponents(year: 2025, month: 4, day: 1))!,
+        endDate: Calendar.current.date(from: DateComponents(year: 2025, month: 4, day: 30))!,
+        isPublic: true,
+        currentProgress: 4,
+        endProgress: 10
+    )
+    
+    static var previews: some View {
+        ProgressGoalDetailView(goal: $goal)
+    }
+}
