9bd47022be2dbaa2d29aa563bc82c9003f7a987c
Yang Sijun <yang@sijun.dev>
2025-04-16 09:39:54 +0900

    Create and Implement GoalTitleCardTextView

---
+++ Projects/Togetree/Sources/Views/GoalTitleCardTextView.swift
@@ -0,0 +1,49 @@
+//
+//  GoalTitleCardTextView.swift
+//  Togetree
+//
+//  Created by 양시준 on 4/16/25.
+//
+
+import SwiftUI
+
+public struct GoalTitleCardTextView: View {
+    var title: String
+    var description: String?
+    
+    public var body: some View {
+        VStack(spacing: 8) {
+            Text(title)
+                .font(.headline)
+                .foregroundStyle(.white)
+            Group {
+                if description != nil {
+                    Text(description ?? "")
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    VStack {
+        GoalTitleCardTextView(
+            title: "앱 배포하기",
+            description: "Togetree 앱 배포하기"
+        )
+        .padding(.horizontal, 20)
+        .padding(.vertical, 32)
+        .frame(maxWidth: .infinity)
+        .background(.gray)
+        
+        GoalTitleCardTextView(
+            title: "앱 배포하기"
+        )
+        .padding(.horizontal, 20)
+        .padding(.vertical, 32)
+        .frame(maxWidth: .infinity)
+        .background(.gray)
+    }
+}
