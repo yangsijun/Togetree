1e221a646fe9322c1555b681787ba1cf4f4a053e
Yang Sijun <yang@sijun.dev>
2025-04-17 11:00:11 +0900

    Create and Implement GoalTreesButtonGroupView

---
+++ Projects/Togetree/Sources/Views/GoalTreesButtonGroupView.swift
@@ -0,0 +1,64 @@
+//
+//  GoalTreesButtonGroupView.swift
+//  Togetree
+//
+//  Created by 양시준 on 4/16/25.
+//
+
+import SwiftUI
+
+public struct GoalTreesButtonGroupView: View {
+    @Binding var isCheering: Bool
+    @Binding var isHelping: Bool
+    
+    public var body: some View {
+        HStack {
+            Button(action: { isCheering.toggle() }) {
+                HStack {
+                    Image(systemName: isCheering ? "hands.clap.fill" : "hands.clap")
+                    Text("응원할게요")
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(
+                    isCheering
+                        ? Capsule()
+                        .fill(Color("SecondaryAccentColor").opacity(0.15))
+                            .stroke(Color("SecondaryAccentColor"), lineWidth: 1)
+                        : Capsule()
+                            .fill(Color.clear)
+                            .stroke(Color("SecondaryAccentColor"), lineWidth: 1)
+                )
+            }
+            Button(action: { isHelping.toggle() }) {
+                HStack {
+                    Image(systemName: isHelping ? "person.2.fill" : "person.2")
+                    Text("도와줄게요")
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(
+                    isHelping
+                        ? Capsule()
+                        .fill(Color("SecondaryAccentColor").opacity(0.15))
+                            .stroke(Color("SecondaryAccentColor"), lineWidth: 1)
+                        : Capsule()
+                            .fill(Color.clear)
+                            .stroke(Color("SecondaryAccentColor"), lineWidth: 1)
+                )
+            }
+        }
+        .tint(Color("SecondaryAccentColor"))
+        .frame(height: 40)
+    }
+}
+
+struct GoalTreesButtonGroupView_Previews: PreviewProvider {
+    @State static var isCheering: Bool = false
+    @State static var isHelping: Bool = false
+    
+    static var previews: some View {
+        GoalTreesButtonGroupView(
+            isCheering: $isCheering,
+            isHelping: $isHelping
+        )
+    }
+}
