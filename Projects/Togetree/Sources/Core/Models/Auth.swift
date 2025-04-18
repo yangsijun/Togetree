2b09ac517f658016e25d440fb93376580faeaadd
Yang Sijun <yang@sijun.dev>
2025-04-19 00:49:37 +0900

    Create Auth Models

---
+++ Projects/Togetree/Sources/Core/Models/Auth.swift
@@ -0,0 +1,21 @@
+//
+//  Auth.swift
+//  Togetree
+//
+//  Created by 양시준 on 4/18/25.
+//
+
+import Foundation
+
+struct AppleLoginRequest: Codable {
+    let userID: String
+    let email: String?
+    let name: String?
+}
+
+struct LoginResponse: Codable {
+    let token: String
+    let userId: UUID
+}
+
+
