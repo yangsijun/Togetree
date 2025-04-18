0e706752fca54da5cc05082a905ba117f45ac08b
Yang Sijun <yang@sijun.dev>
2025-04-19 00:51:27 +0900

    Create AuthService

---
+++ Projects/Togetree/Sources/Core/Network/AuthService.swift
@@ -0,0 +1,19 @@
+//
+//  AuthService.swift
+//  Togetree
+//
+//  Created by 양시준 on 4/18/25.
+//
+
+import Foundation
+
+class AuthService {
+    // TODO: API URL
+//    let baseURL = URL(string: "")!
+    
+//    func loginWithApple(userID: String, email: String?, name: String?) async throws -> LoginResponse {
+//        let url = baseURL.appendingPathComponent("/auth/apple-login")
+//        let request = AppleLoginRequest(userID: userID, email: email, name: name)
+//        return try await APIClient.post(url, body: request)
+//    }
+}
