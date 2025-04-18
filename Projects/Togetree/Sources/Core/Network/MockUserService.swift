37df41428ba7343841948094c174aaccb12855ab
Yang Sijun <yang@sijun.dev>
2025-04-19 00:51:01 +0900

    Create and Implement UserService and MockUserService

---
+++ Projects/Togetree/Sources/Core/Network/MockUserService.swift
@@ -0,0 +1,32 @@
+//
+//  MockUserService.swift
+//  Togetree
+//
+//  Created by 양시준 on 4/18/25.
+//
+
+import Foundation
+
+var mockUserList: [User] = [
+    User(id: UUID(uuidString: "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee0")!, name: "Air", profileImageUrl: "https://picsum.photos/200/300", statusMessage: "Proverbs 3:5-6"),
+    User(id: UUID(uuidString: "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee1")!, name: "Ella", profileImageUrl: "https://picsum.photos/200/300", statusMessage: "Hello, World!"),
+    User(id: UUID(uuidString: "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee2")!, name: "Mingky", profileImageUrl: "https://picsum.photos/200/300", statusMessage: "밍기적..."),
+    User(id: UUID(uuidString: "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee3")!, name: "Moo", profileImageUrl: "https://picsum.photos/200/300", statusMessage: "안녕")
+]
+
+class MockUserService {
+    // TODO: API URL
+//    let baseURL = URL(string: "")!
+
+    func fetchUser(id: UUID) async throws -> User {
+        return mockUserList.first(where: { $0.id == id })!
+    }
+
+    func loadFollowings(of userId: UUID) async throws -> [User] {
+        return mockUserList
+    }
+
+    func loadFollowers(of userId: UUID) async throws -> [User] {
+        return mockUserList
+    }
+}
---
+++ Projects/Togetree/Sources/Core/Network/UserService.swift
@@ -0,0 +1,28 @@
+//
+//  UserService.swift
+//  Togetree
+//
+//  Created by 양시준 on 4/18/25.
+//
+
+import Foundation
+
+//class UserService {
+//    // TODO: API URL
+//    let baseURL = URL(string: "")!
+//    
+//    func fetchUser(id: UUID) async throws -> User {
+//        let url = baseURL.appendingPathComponent("users/\(id.uuidString)")
+//        return try await APIClient.get(url)
+//    }
+//    
+//    func loadFollowings(of userId: UUID) async throws -> [User] {
+//        let url = baseURL.appendingPathComponent("users/\(userId.uuidString)/followings")
+//        return try await APIClient.get(url)
+//    }
+//    
+//    func loadFollowers(of userId: UUID) async throws -> [User] {
+//        let url = baseURL.appendingPathComponent("users/\(userId.uuidString)/followers")
+//        return try await APIClient.get(url)
+//    }
+//}
