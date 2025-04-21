a68c008f6c3f996a07c695ec36944f505513f0e2
Yang Sijun <yang@sijun.dev>
2025-04-21 17:20:21 +0900

    Implement UserProfileImageView

--- Projects/Togetree/Sources/Features/Social/Views/UserProfileImageView.swift
+++ Projects/Togetree/Sources/Features/Social/Views/UserProfileImageView.swift
@@ -8,28 +8,36 @@
 import SwiftUI
 
 struct UserProfileImageView: View {
+    @State private var loader = ImageLoader()
     var user: User
     var imageSize: CGFloat
     
     var body: some View {
         Group {
-            VStack {
-                AsyncImage(url: URL(string: user.profileImageUrl ?? "")) { image in
-                    image
+            Group {
+                if let image = loader.image {
+                    Image(uiImage: image)
                         .resizable()
                         .scaledToFill()
                         .frame(width: imageSize, height: imageSize)
-                } placeholder: {
+                } else {
                     Image(systemName: "person.circle.fill")
                         .resizable()
                         .scaledToFill()
                         .frame(width: imageSize, height: imageSize)
-                        }
-                .clipShape(Circle())
-                .overlay(
-                    Circle()
-                        .stroke(Color.gray.opacity(0.2), lineWidth: 1)
-                )
+                }
+            }
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.gray.opacity(0.2), lineWidth: 1))
+            .onAppear {
+                if let url = URL(string: user.profileImageUrl ?? "") {
+                    loader.load(from: url)
+                }
+            }
+            .onChange(of: user) {
+                if let url = URL(string: user.profileImageUrl ?? "") {
+                    loader.load(from: url)
+                }
             }
         }
     }
@@ -39,17 +47,40 @@ struct UserProfileImageView_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
             UserProfileImageView(
-                user: .init(name: "Test User", profileImageUrl: "https://picsum.photos/200/300"),
+                user: mockUserList[0],
                 imageSize: 100
             )
             UserProfileImageView(
-                user: .init(name: "Test User", profileImageUrl: "https://picsum.photos/200/300"),
+                user: mockUserList[0],
                 imageSize: 64
             )
             UserProfileImageView(
-                user: .init(name: "Test User", profileImageUrl: "https://picsum.photos/200/300"),
+                user: mockUserList[0],
                 imageSize: 32
             )
+//            UserProfileImageView(
+//                user: .init(name: "Test User", profileImageUrl: ""),
+//                imageSize: 32
+//            )
         }
+    }
+}
+
+@Observable class ImageLoader {
+    var image: UIImage?
+    var isFailed = false
+
+    func load(from url: URL) {
+        isFailed = false
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                if let data = data, let img = UIImage(data: data) {
+                    self.image = img
+                } else {
+                    self.isFailed = true
+                }
+            }
+        }
+        task.resume()
     }
 }
